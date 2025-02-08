31fafb726c18cd44f9052fa1b52e914a2c235ef0
Rahul Patel <rpatel@nseit.com>
2025-02-08 11:14:23 +0530

    intial commit

---
+++ src/main/java/com/example/demoopenshift/DemoopenshiftApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoopenshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoopenshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoopenshiftApplication.class, args);
+	}
+
+}
