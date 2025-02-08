1782279bdb93a230b12138d063c589a53316c2b2
Rahul Patel <rpatel@nseit.com>
2025-02-08 11:20:30 +0530

    added rest end point /hello

--- src/main/java/com/example/demoopenshift/DemoopenshiftApplication.java
+++ src/main/java/com/example/demoopenshift/DemoopenshiftApplication.java
@@ -2,12 +2,20 @@ package com.example.demoopenshift;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoopenshiftApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoopenshiftApplication.class, args);
 	}
+	
+	@RequestMapping("/hello")
+    public String hello() {
+        return "Hello welcome poc of openshift...!!!!!!!";
+    }  
 
 }
